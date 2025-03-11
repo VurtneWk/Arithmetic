f0299fd7d237877051a8b74544910886c0daca21
vurtnewk <vurtnewk@163.com>
2025-03-11 15:10:12 +0800

    07线性表-单向循环链表-常用操作接口

--- Cpp/03.单向循环链表/03-单向循环链表.cpp
+++ Cpp/03.单向循环链表/03-单向循环链表.cpp
@@ -4,14 +4,14 @@
 using namespace std;
 
 // 单向循环列表
-class CirCleLink {
+class CircleLink {
    public:
-    CirCleLink() {
+    CircleLink() {
         head_ = new Node();
         tail_ = head_;
         head_->next_ = head_;
     }
-    ~CirCleLink() {
+    ~CircleLink() {
         Node* p = head_->next_;
         while (p != head_) {
             // 头 指向 p的下一个节点
@@ -35,7 +35,7 @@ class CirCleLink {
         Node* node = new Node(val);
         node->next_ = head_->next_;
         head_->next_ = node;
-        if (node->next_ == head) {
+        if (node->next_ == head_) {
             tail_ = node;
         }
     }
@@ -71,7 +71,7 @@ class CirCleLink {
         return false;
     }
     // 打印链表
-    void Show() const {
+    void show() const {
         Node* p = head_->next_;
         while (p != head_) {
             cout << p->data_ << " ";
@@ -90,7 +90,8 @@ class CirCleLink {
     Node* head_;
     Node* tail_;
 };
-
+// 测试 单向循环链表
+#if 0
 int main() {
     CircleLink clink;
     srand(time(0));
@@ -101,11 +102,75 @@ int main() {
     }
 
     clink.insertTail(200);
-    clink.Show();
+    clink.show();
 
     clink.remove(200);
     clink.show();
 
     clink.insertTail(300);
     clink.show();
+}
+#endif
+
+// 约瑟夫环问题
+
+/**
+ * 约瑟夫环是一个数学的应用问题：已知n个人（以编号1，2，3...n分别表示）围坐在一张圆桌周围，
+ * 从编号为k的人开始报数，数到m的那个人出列，它的下一个人又从1开始报数，数到m的那个人又出列，
+ * 依此规律重复下去，直到圆桌周围的人全部出列，输出人的出列顺序。
+ */
+
+struct Node {
+    Node(int data = 0) : data_(data), next_(nullptr) {}
+    int data_;
+    Node* next_;
+};
+
+void joseph(Node* head, int k, int m) {
+    Node* p = head;
+    Node* q = head;  // 目的是指向p的前一个
+    // q指向最后一个
+    while (q->next_ != head) {
+        q = q->next_;
+    }
+    // 先找到k， 然后开始数
+    for (int i = 1; i < k; i++) {
+        q = p;
+        p = p->next_;
+    }
+    for (;;) {
+        for (int i = 1; i < m; i++) {
+            q = p;
+            p = p->next_;
+        }
+        cout << p->data_ << " ";
+        if (q == p) {
+            delete p;
+            break;
+        }
+        q->next_ = p->next_;
+        delete p;
+        p = q->next_;
+    }
+}
+int main() {
+    Node* head = new Node(1);
+    Node* n2 = new Node(2);
+    Node* n3 = new Node(3);
+    Node* n4 = new Node(4);
+    Node* n5 = new Node(5);
+    Node* n6 = new Node(6);
+    Node* n7 = new Node(7);
+    Node* n8 = new Node(8);
+
+    head->next_ = n2;
+    n2->next_ = n3;
+    n3->next_ = n4;
+    n4->next_ = n5;
+    n5->next_ = n6;
+    n6->next_ = n7;
+    n7->next_ = n8;
+    n8->next_ = head;
+
+    joseph(head, 1, 1);
 }
--- leetcode/61.旋转链表.cpp
+++ leetcode/61.旋转链表.cpp
@@ -4,6 +4,14 @@
  * [61] 旋转链表
  */
 
+struct ListNode {
+    int val;
+    ListNode* next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode* next) : val(x), next(next) {}
+};
+
 // @lc code=start
 /**
  * Definition for singly-linked list.
@@ -15,6 +23,7 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
+
 class Solution {
    public:
     ListNode* rotateRight(ListNode* head, int k) {
