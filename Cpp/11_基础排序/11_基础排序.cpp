3e90fff67976431270f775fb162ddaabbf3edc58
vurtnewk <vurtnewk@163.com>
2025-03-13 23:42:39 +0800

    基础排序

--- Cpp/11_基础排序/11_基础排序.cpp
+++ Cpp/11_基础排序/11_基础排序.cpp
@@ -30,6 +30,65 @@ void bubbleSort(int arr[], int size) {
     }
 }
 
+/**
+ * 选择排序
+ */
+void choiceSort(int arr[], int size) {
+    for (int i = 0; i < size - 1; i++) {
+        // 记录最小值索引
+        int index = i;
+        // 每次是从记录的元素的下一个开始
+        for (int j = i + 1; j < size; j++) {
+            if (arr[j] < arr[index]) {
+                index = j;
+            }
+        }
+        // 如果找到和一开始记录的最小值不一样，则进行交换
+        if (index != i) {
+            int tmp = arr[i];
+            arr[i] = arr[index];
+            arr[index] = tmp;
+        }
+    }
+}
+
+/**
+ * 插入排序
+ */
+void insertSort(int arr[], int size) {
+    // 从第一个开始
+    for (int i = 1; i < size; i++) {
+        int val = arr[i];  // 要比较的值
+        int j = i - 1;
+        // 和前面的进行比较
+        while (j >= 0 && arr[j] > val) {
+            arr[j + 1] = arr[j];  // 如果前面的值大于当前值，后移
+            j--;
+        }
+        arr[j + 1] = val;  // 找到位置后插入
+    }
+}
+
+/**
+ * 希尔排序
+ */
+void shellSort(int arr[], int size) {
+    for (int gap = size / 2; gap > 0; gap = gap / 2) {
+        // 从第一个开始
+        for (int i = gap; i < size; i++) {
+            int val = arr[i];  // 要比较的值
+            int j = i - gap;
+            // 和前面的进行比较
+            while (j >= 0 && arr[j] > val) {
+                arr[j + gap] = arr[j];  // 如果前面的值大于当前值，后移
+                j -= gap;
+            }
+            arr[j + gap] = val;  // 找到位置后插入
+        }
+    }
+}
+
+#if 0
 int main() {
     int arr[10];
     srand(time(nullptr));
@@ -42,11 +101,52 @@ int main() {
     }
     cout << endl;
 
-    bubbleSort(arr, 10);
+    shellSort(arr, 10);
     for (auto &&i : arr) {
         cout << i << " ";
     }
     cout << endl;
 
     return 0;
+}
+#endif
+
+int main() {
+    const int COUNT = 100000;
+    int* arr = new int[COUNT];
+    int* brr = new int[COUNT];
+    int* crr = new int[COUNT];
+    int* drr = new int[COUNT];
+
+    srand(time(NULL));
+
+    for (int i = 0; i < COUNT; i++) {
+        int val = rand() % COUNT;
+        arr[i] = val;
+        brr[i] = val;
+        crr[i] = val;
+        drr[i] = val;
+    }
+
+    clock_t begin, end;
+
+    begin = clock();
+    bubbleSort(arr, COUNT);
+    end = clock();
+    cout << "BubbleSort spend:" << (end - begin) * 1.0 / CLOCKS_PER_SEC << "s" << endl;
+
+    begin = clock();
+    choiceSort(brr, COUNT);
+    end = clock();
+    cout << "ChoiceSort spend:" << (end - begin) * 1.0 / CLOCKS_PER_SEC << "s" << endl;
+
+    begin = clock();
+    insertSort(crr, COUNT);
+    end = clock();
+    cout << "InsertSort spend:" << (end - begin) * 1.0 / CLOCKS_PER_SEC << "s" << endl;
+
+    begin = clock();
+    shellSort(drr, COUNT);
+    end = clock();
+    cout << "ShellSort spend:" << (end - begin) * 1.0 / CLOCKS_PER_SEC << "s" << endl;
 }
